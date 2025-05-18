ff122c3c50e700e86e526d1de09febd4a8ffa568
StoolMonster <lfw93814@gmail.com>
2025-05-18 10:48:04 +0800

    Init

---
+++ AlienMacroKeys/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by AlienMacroKeys.rc
+//
+#define IDC_MYICON                      2
+#define IDD_ALIENMACROKEYS_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_ALIENMACROKEYS              107
+#define IDI_SMALL                       108
+#define IDC_ALIENMACROKEYS              109
+#define IDR_MAINFRAME                   128
+#define IDD_MAINWINDOW                  129
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
